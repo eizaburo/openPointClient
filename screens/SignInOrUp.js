63224d2cc34ce3ca57c4aa219a473b080998bfb8
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2019-09-26 08:40:58 +0900

    SignIn, SignUp画面レイアウト

--- screens/SignInOrUp.js
+++ screens/SignInOrUp.js
@@ -5,18 +5,32 @@ import { Card, Input, Button } from 'react-native-elements';
 class SignInOrUp extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>SignInOrUp</Text>
-                <Button
+            <View style={{ flex: 1 }}>
+                <Card
                     title="ログイン"
-                    buttonStyle={{ marginTop: 10 }}
-                    onPress={() => this.props.navigation.navigate('SignedIn')}
-                />
-                <Button
-                    title="新規登録"
-                    buttonStyle={{ marginTop: 10 }}
-                    onPress={() => this.props.navigation.navigate('SignUp')}
-                />
+                    containerStyle={{ marginTop: 20 }}
+                >
+                    <Input
+                        label="Email"
+                        labelStyle={{ marginTop: 10 }}
+                        autoCapitalize="none"
+                    />
+                    <Input
+                        label="パスワード"
+                        labelStyle={{ marginTop: 10 }}
+                        secureTextEntry
+                    />
+                    <Button
+                        title="ログイン"
+                        buttonStyle={{ marginTop: 20 }}
+                        onPress={() => this.props.navigation.navigate('SignedIn')}
+                    />
+                    <Button
+                        title="新規登録はこちら"
+                        buttonStyle={{ marginTop: 20, backgroundColor: "#3cb371" }}
+                        onPress={() => this.props.navigation.navigate('SignUp')}
+                    />
+                </Card>
             </View>
         );
     }
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -5,13 +5,37 @@ import { Card, Input, Button } from 'react-native-elements';
 class SignUp extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>SignUp</Text>
-                <Button
+            <View style={{ flex: 1 }}>
+                <Card
                     title="新規登録"
-                    buttonStyle={{ marginTop: 10 }}
-                    onPress={() => this.props.navigation.navigate('SignedIn')}
-                />
+                    containerStyle={{ marginTop: 20 }}
+                >
+                    <Input
+                        label="Email（必須）"
+                        labelStyle={{ marginTop: 10 }}
+                        autoCapitalize="none"
+                    />
+                    <Input
+                        label="パスワード（必須）"
+                        labelStyle={{ marginTop: 10 }}
+                        secureTextEntry
+                    />
+                    <Input
+                        label="パスワード再確認（必須）"
+                        labelStyle={{ marginTop: 10 }}
+                        secureTextEntry
+                    />
+                    <Input
+                        label="電話番号（オプション）"
+                        labelStyle={{ marginTop: 10 }}
+                        autoCapitalize="none"
+                    />
+                    <Button
+                        title="新規登録"
+                        buttonStyle={{ marginTop: 20, backgroundColor: "#3cb371" }}
+                        onPress={() => this.props.navigation.navigate('SignedIn')}
+                    />
+                </Card>
             </View>
         );
     }
